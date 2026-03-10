e3557717b4d1f217ac4394c1e3cbece2d59caf06
lucassantooss <lucassantosalvarez20@gmail.com>
2026-03-10 12:32:50 +0100

    Continuacion ejercicios 5, 6, 7

--- src/ej4/Cuenta.java
+++ src/ej4/Cuenta.java
@@ -3,25 +3,35 @@ package ej4;
 public class Cuenta {
 
 private double saldo;
+private Cliente titular;
 
-public Cuenta(double saldo){
+public Cuenta(Cliente titular, double saldo){
+    this.titular=titular;
     this.saldo=saldo;
     if(saldo<0){
-        saldo=0;
+        this.saldo=0;
     }
 }
 
-public void ingresar(int cantidad){
-    saldo+=cantidad;
-    System.out.println("Ingreso realizado con exito.");
+public void ingresar(double cantidad){
+    if(cantidad<=0){
+        System.out.println("No se puede ingresar esa cantidad.");
+    }else {
+        this.saldo += cantidad;
+        System.out.println("Ingreso realizado con exito.");
+    }
 }
 
-public void retirar(int cantidad){
-    saldo-=cantidad;
-    System.out.println("Retirada realizada con exito.");
+public void retirar(double cantidad){
+    if(cantidad<=this.saldo){
+        this.saldo-=cantidad;
+        System.out.println("Retirada realizada con éxito");
+    }else{
+        System.out.println("No dispones de dinero suficiente para realizar la retirada.");
+    }
 }
-public void mosrtarInfo(){
-    System.out.println("Saldo actual: "+saldo);
+public void mostrarInfo(){
+    System.out.println("Saldo actual: "+this.saldo);
 }
 
 }
--- src/ej4/Main.java
+++ src/ej4/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
     Cliente uno=new Cliente("Lucas", 12345678);
-    Cuenta nueva=new Cuenta(120);
+    Cuenta nueva=new Cuenta(uno,120);
 
     uno.mostrarCliente();
+    nueva.mostrarInfo();
     nueva.ingresar(20);
     nueva.retirar(120);
-    nueva.mosrtarInfo();
+    nueva.mostrarInfo();
 
 
 
